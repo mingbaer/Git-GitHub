2dfa8e06510daf098fc4265deaebbad66920af0e
Alexandra Baer Chan <mingbaer@gmail.com>
2024-04-21 12:28:47 -0500

    Initial Commit

---
+++ Git & GitHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git & GitHub
+//
+//  Created by Alexandra Baer Chan on 4/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
